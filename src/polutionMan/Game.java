a5daa5d5fccfccc4ff4b59c918949789835516cf
Cu de Apito <joaozinhobaodebriga@gmail.com>
2025-05-08 17:30:23 -0300

    Atualizando o código para poder mexer o personagem

--- src/polutionMan/Game.java
+++ src/polutionMan/Game.java
@@ -1,5 +1,26 @@
 package polutionMan;
 
-public class Game {
+import javax.swing.JPanel;
 
+public class Game extends JPanel implements Runnable{
+
+	Thread gameThread;
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	@Override
+	public void run() {
+		//ISSO VAI CRIAR O NOSSO LOOP NO JOGO
+		//UMA DAS COISAS IMPORTANTES PARA O JOGO
+		while(gameThread !=null) {
+			//UPDATE: atualiza a informação da posição do personagem
+			
+			//DRAW: desenha na tela a informação atualizada
+		}
+		
+	}	
 }
