28749899f46d1deeed8223a656d9725afc162761
Cu de Apito <joaozinhobaodebriga@gmail.com>
2025-05-15 18:02:45 -0300

    Todo código para poder movimentar

--- src/polutionMan/ControleTeclado.java
+++ src/polutionMan/ControleTeclado.java
@@ -1,5 +1,59 @@
 package polutionMan;
 
-public class ControleTeclado {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ControleTeclado implements KeyListener{
+	
+	public boolean upPressed, downPressed, rightPressed, leftPressed;
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W) {
+			upPressed = true;
+		}
+		if(code == KeyEvent.VK_S) {
+			downPressed = true;
+		}
+		if(code == KeyEvent.VK_D) {
+			rightPressed = true;
+		}
+		if(code == KeyEvent.VK_A) {
+			leftPressed = true;
+		}
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		int code = e.getKeyCode();
+		
+		if(code == KeyEvent.VK_W) {
+			upPressed = false;
+		}
+		if(code == KeyEvent.VK_S) {
+			downPressed = false;
+		}
+		if(code == KeyEvent.VK_D) {
+			rightPressed = false;
+		}
+		if(code == KeyEvent.VK_A) {
+			leftPressed = false;
+		}
+		
+		
+	}
+	
 
 }
--- src/polutionMan/Game.java
+++ src/polutionMan/Game.java
@@ -1,10 +1,29 @@
 package polutionMan;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements Runnable{
-
+	
+	
+	//FPS DO JOGO
+	int FPS = 60;
+	
+	ControleTeclado keyH = new ControleTeclado();
 	Thread gameThread;
+	
+	int speedPlayer = 4;
+	//posição de origem do jogador
+	int playerX = 0;
+	int playerY = 0;
+	
+	public Game() {
+		
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+		
+	}
 	
 	public void startGameThread() {
 		
@@ -16,11 +35,56 @@ public class Game extends JPanel implements Runnable{
 	public void run() {
 		//ISSO VAI CRIAR O NOSSO LOOP NO JOGO
 		//UMA DAS COISAS IMPORTANTES PARA O JOGO
+		
+		//AGORA VOU CRIAR O LOOP
+		
+		double drawInterval = 1000000000 / FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
 		while(gameThread !=null) {
 			//UPDATE: atualiza a informação da posição do personagem
 			
-			//DRAW: desenha na tela a informação atualizada
+			currentTime = System.nanoTime();			
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			
+			lastTime = currentTime;
+			
+			if(delta >=1) {
+				update();
+				
+				
+				//DRAW: desenha na tela a informação atualizada
+				repaint();
+				delta--;
+				
+			}
+			
+			
 		}
 		
 	}	
+	public void update() {
+		
+		if(keyH.upPressed == true) {
+			playerY -= speedPlayer;
+		}
+		else if(keyH.downPressed == true) {
+			playerY += speedPlayer;
+		}
+		else if(keyH.rightPressed == true) {
+			playerX += speedPlayer;
+		}
+		else if(keyH.leftPressed == true) {
+			playerX -= speedPlayer;
+		}
+	}
+	//possivelmente os lugares que vão estar o sprites
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+	}
+	
 }
